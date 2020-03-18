5f26797b87663a31d54699465f370f9e0077d973
Keithw12 <keithwilcoxr@gmail.com>
2020-03-17 20:20:01 -0500

    signatures.py
    implementation of key pair generation, signing, and verification of messages.

--- signatures.py
+++ signatures.py
@@ -1,5 +1,19 @@
-import Crypto.PublicKey
+import Crypto
 from Crypto.PublicKey import RSA
 from Crypto import Random
 import base64
 
+# Key pair generation
+def rsakeys():  
+     length=1024  
+     privatekey = RSA.generate(length, Random.new().read)  
+     publickey = privatekey.publickey()  
+     return privatekey, publickey
+
+# Signing a message
+def sign(privatekey,data):
+    return base64.b64encode(str((privatekey.sign(data,''))[0]).encode())
+
+# Verify a message
+def verify(publickey,data,sign):
+     return publickey.verify(data,(int(base64.b64decode(sign)),))
